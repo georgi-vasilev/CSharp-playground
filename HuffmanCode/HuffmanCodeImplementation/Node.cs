c49fbf5523a47f8cef360af394e2dfc2fa549e2e
Georgi Vasilev <35624206+georgi-vasilev@users.noreply.github.com>
2021-11-06 16:15:46 +0200

    Huffman code implementation

---
+++ HuffmanCode/HuffmanCodeImplementation/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise6
+{
+    public class Node
+    {
+        public char Letter { get; set; }
+        
+        public int Frequency { get; set; }
+
+        public Node LeftNode { get; set; }
+
+        public Node RightNode { get; set; }
+    }
+}
